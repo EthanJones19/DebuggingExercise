b6294e76b6c7a548d2937c41d8cbcc901583d46a
Ethan Jones <ethanjones800@yahoo.com>
2020-09-06 20:45:15 -0500

    Fixed battle system. Fixed the name cycle.

--- Game.cs
+++ Game.cs
@@ -11,7 +11,8 @@ namespace HelloWorld
         int _playerHealth = 120;
         int _playerDamage = 20;
         int _playerDefense = 10;
-        int levelScaleMax = 5;
+        int _levelScaleMax = 5;
+        int _roomNum = 0;
         //Run the game
         public void Run()
         {
@@ -65,7 +66,7 @@ namespace HelloWorld
             }
 
             //Loops until the player or the enemy is dead
-            while(_playerHealth >= 0 || enemyHealth >= 0)
+            while(_playerHealth >= 0 && enemyHealth >= 0)
             {
                 //Displays the stats for both charactersa to the screen before the player takes their turn
                 PrintStats(_playerName, _playerHealth, _playerDamage, _playerDefense);
@@ -73,55 +74,55 @@ namespace HelloWorld
 
                 //Get input from the player
                 char input = ' ';
-                Console.WriteLine("Attack", "Defend");
+                GetInput(out input, "Attack", "Defend");
+                Console.WriteLine();
                 //If input is 1, the player wants to attack. By default the enemy blocks any incoming attack
                 if(input == '1')
                 {
-                    enemyHealth -= _playerDamage;
-                    BlockAttack(enemyHealth, _playerDamage, enemyDefense);
-                    Console.WriteLine("You dealt " + _playerDamage + " damage.");
+                    BlockAttack(ref enemyHealth, _playerDamage, enemyDefense);
+                    Console.WriteLine(" You dealt " + (_playerDamage - enemyDefense) + " damage.");
                     Console.Write("> ");
                     Console.ReadKey();
+                    Console.WriteLine(enemyName + " dealt " + enemyAttack + " damage.");
+                    _playerHealth -= enemyAttack;
+
                 }
                 //If the player decides to defend the enemy just takes their turn. However this time the block attack function is
                 //called instead of simply decrementing the health by the enemy's attack value.
                 else
                 {
-                    BlockAttack(_playerHealth, enemyAttack, _playerDefense);
-                    Console.WriteLine(enemyName + " dealt " + enemyAttack + " damage.");
+                    BlockAttack(ref _playerHealth, enemyAttack, _playerDefense);
+                    Console.WriteLine(enemyName + " dealt " + (enemyAttack - _playerDefense) + " damage.");
                     Console.Write("> ");
                     Console.ReadKey();
-                    turnCount++;
                     Console.Clear();
                 }
                 Console.Clear();
                 //After the player attacks, the enemy takes its turn. Since the player decided not to defend, the block attack function is not called.
-                _playerHealth -= enemyAttack;
-                Console.WriteLine(enemyName + " dealt " + enemyAttack + " damage.");
-                Console.Write("> ");
                 Console.ReadKey();
+
                 turnCount++;
                 
             }
             //Return whether or not our player died
-            return _playerHealth != 0;
+            return _playerHealth >= 0;
 
         }
         //Decrements the health of a character. The attack value is subtracted by that character's defense
-        void BlockAttack(int opponentHealth, int attackVal, int opponentDefense)
+        void BlockAttack(ref int opponentHealth, int attackVal, int opponentDefense)
         {
             int damage = attackVal - opponentDefense;
             if(damage < 0)
             {
                 damage = 0;
             }
-            opponentHealth -= damage;
+             opponentHealth -= damage;
         }
         //Scales up the player's stats based on the amount of turns it took in the last battle
         void LevelUp(int turnCount)
         {
             //Subtract the amount of turns from our maximum level scale to get our current level scale
-            int scale = levelScaleMax - turnCount;
+            int scale = _levelScaleMax - turnCount;
             if(scale <= 0)
             {
                 scale = 1;
@@ -138,11 +139,13 @@ namespace HelloWorld
             //Initialize input
             input = ' ';
             //Loop until the player enters a valid input
-            while(input != '1' && input != '2')
+            while (input != '1' && input != '2')
+            {
                 Console.WriteLine("1." + option1);
                 Console.WriteLine("2." + option2);
                 Console.Write("> ");
                 input = Console.ReadKey().KeyChar;
+            }
         }
 
         //Prints the stats given in the parameter list to the console
@@ -186,7 +189,7 @@ namespace HelloWorld
             if(StartBattle(roomNum, ref turnCount))
             {
                 LevelUp(turnCount);
-                ClimbLadder(roomNum++);
+                return;
             }
             _gameOver = true;
 
@@ -260,8 +263,8 @@ namespace HelloWorld
         //Repeated until the game ends
         public void Update()
         {
-            SelectCharacter();
-            ClimbLadder(0);
+            ClimbLadder(_roomNum);
+            _roomNum++;
         }
 
         //Performed once when the game ends
